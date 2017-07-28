3b83e3dbfc13ebd713bf97ac716bceb5c0118668
Hollis Chui <hollisc@Holliss-MacBook-Pro.local>
2017-07-28 11:24:16 -0500

    Fix failing test in canary spec.

--- server/00-canary/canary.spec.js
+++ server/00-canary/canary.spec.js
@@ -1,5 +1,6 @@
 describe('the canary spec', () => {
-  it('shows the infrastructure works', () => {
-    false.should.be.true();
+  it.only('shows the infrastructure works', () => {
+    true.should.be.true('This is my custom diagnostic message.');
   });
 });
+

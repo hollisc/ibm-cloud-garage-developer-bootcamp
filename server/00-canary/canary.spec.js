b93fb762dc5c3122e0ad70218a46d214cf53b012
Hollis Chui <hollisc@Holliss-MacBook-Pro.local>
2017-07-28 11:56:03 -0500

    Make first test pass

--- server/00-canary/canary.spec.js
+++ server/00-canary/canary.spec.js
@@ -1,6 +1,5 @@
 describe('the canary spec', () => {
-  it.only('shows the infrastructure works', () => {
+  it('shows the infrastructure works', () => {
     true.should.be.true('This is my custom diagnostic message.');
   });
 });
-

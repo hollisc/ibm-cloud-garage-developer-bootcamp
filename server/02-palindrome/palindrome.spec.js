28a051df8d71c171c9c52dbf43088f7420fcf787
Hollis Chui <hollisc@holliss-mbp.dfw.ibm.com>
2017-07-28 14:38:46 -0500

    Make first palindrom test pass

--- server/02-palindrome/palindrome.spec.js
+++ server/02-palindrome/palindrome.spec.js
@@ -3,3 +3,18 @@ describe('the palindrome canary spec', () => {
       true.should.be.true();
     });
 });
+
+function isPalindrome() {
+    return true;
+  }
+
+describe('a palindrome', () => {
+  it('is racecar', () => {
+    isPalindrome('racecar').should.be.true();
+  });
+  it('is not racecars');
+  it('is not race car');
+  it('"   " is not a palindrome');
+  it('"" is not a palindrome');
+  it('is "mom dad mom"');
+});

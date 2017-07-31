6105a3da9dccb3f21919453e30f2c5d8ac62041b
Hollis Chui <hollisc@holliss-mbp.dfw.ibm.com>
2017-07-31 16:36:19 -0500

    Create remaining tests for PrimeFactors.

--- server/04-prime-numbers/prime-numbers.spec.js
+++ server/04-prime-numbers/prime-numbers.spec.js
@@ -1,6 +1,12 @@
+/* eslint-disable no-param-reassign */
 function primeFactorsOf(number) {
-  if (number === 1) return [];
-  return [number];
+  const factors = [];
+  let divisor = 2;
+  while (number > 1) {
+    for (; number % divisor === 0; number /= divisor) factors.push(divisor);
+    divisor++;
+  }
+  return factors;
 }
 
 describe.only('the prime numbers canary spec', () => {
@@ -15,10 +21,40 @@ describe.only('the prime numbers canary spec', () => {
   it('return 3 for 3', () => {
     primeFactorsOf(3).should.deepEqual([3]);
   });
-  it('return 2, 2 for 4');
-  it('return 5 for 5');
-  it('return 2, 3 for 6');
-  it('return 7 for 7');
-  it('return 2, 2, 2 for 8');
-  it('return 3, 3 for 9');
+
+  it('return 2, 2 for 4', () => {
+    primeFactorsOf(4).should.deepEqual([2, 2]);
+  });
+
+  it('return 5 for 5', () => {
+    primeFactorsOf(5).should.deepEqual([5]);
+  });
+
+  it('return 2, 3 for 6', () => {
+    primeFactorsOf(6).should.deepEqual([2, 3]);
+  });
+
+  it('return 7 for 7', () => {
+    primeFactorsOf(7).should.deepEqual([7]);
+  });
+
+  it('return 2, 2, 2 for 8', () => {
+    primeFactorsOf(8).should.deepEqual([2, 2, 2]);
+  });
+
+  it('return 3, 3 for 9', () => {
+    primeFactorsOf(9).should.deepEqual([3, 3]);
+  });
+
+  it('return [2, 2, 5, 5] for 100', () => {
+      primeFactorsOf(100).should.deepEqual([2, 2, 5, 5]);
+    });
+
+  it('return [2017] for 2017', () => {
+        primeFactorsOf(2017).should.deepEqual([2017]);
+      });
+
+  it('return [2, 2, 2, 2, 2, 3, 3, 7] for 2016', () => {
+         primeFactorsOf(2016).should.deepEqual([2, 2, 2, 2, 2, 3, 3, 7]);
+       });
 });

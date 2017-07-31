637849b9197a51a54914c76ff622b16d312b2ede
Hollis Chui <hollisc@holliss-mbp.dfw.ibm.com>
2017-07-31 11:03:19 -0500

    Create fifth stack test.

--- server/03-stack/stack.spec.js
+++ server/03-stack/stack.spec.js
@@ -46,8 +46,12 @@ describe.only('the stack spec', () => {
     stack.isEmpty().should.be.true();
   });
 
+  it('leaves stack size 0 when pushed and popped', () => {
+    stack.push();
+    stack.pop();
+    stack.size().should.equal(0);
+  });
 
-  it('leaves stack size 0 when pushed and popped');
   it('overflows');
   it('under-flows');
   it('pops the same one pushed');
